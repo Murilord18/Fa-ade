f5c885d197e8647531372a976b66800447ea4196
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:08:54 -0300

    Update Credito.Java

--- src/main/java/br/com/banco/facade/Credito.java
+++ src/main/java/br/com/banco/facade/Credito.java
@@ -1,4 +1,17 @@
 package br.com.banco.facade;
 
-public class Credito {
+public class Credito extends Departamento{
+
+    private static final Credito instancia = new Credito();
+
+    private Credito() {}
+
+    public static Credito getInstancia() {
+        return instancia;
+    }
+
+    @Override
+    public String getNome() {
+        return "Departamento de Crédito";
+    }
 }
