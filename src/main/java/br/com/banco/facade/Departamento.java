a04e742b9ba02e99b23f7292b9968ac658e98b94
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:02:58 -0300

    Add Metodos Departamento

--- src/main/java/br/com/banco/facade/Departamento.java
+++ src/main/java/br/com/banco/facade/Departamento.java
@@ -1,7 +1,25 @@
 package br.com.banco.facade;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Departamento {
 
+    private List<Conta> contasComPendencia = new ArrayList<>();
 
+    public void adicionarContaPendente(Conta conta) {
+        this.contasComPendencia.add(conta);
+    }
+
+    public void removerPendencia(Conta conta) {
+        this.contasComPendencia.remove(conta);
+    }
+
+    public boolean verificarContaComPendencia(Conta conta) {
+        return this.contasComPendencia.contains(conta);
+    }
+
+
+    public abstract String getNome();
+}
