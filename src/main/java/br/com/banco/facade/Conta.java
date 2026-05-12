3581ed1ed9e338f6dd17cc76714fdaf5d17452e0
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:30:24 -0300

    Update Conta.Java

--- src/main/java/br/com/banco/facade/Conta.java
+++ src/main/java/br/com/banco/facade/Conta.java
@@ -1,6 +1,46 @@
 package br.com.banco.facade;
 
+
 public class Conta {
 
+    private String numero;
+    private String titular;
+    private double saldo;
 
+    public Conta(String numero, String titular, double saldoInicial) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+
+    public boolean solicitarEncerramento() {
+        return BancoFacade.verificarPendenciasEncerramento(this);
+    }
+
+
+    public boolean solicitarEmprestimo(double valor) {
+        return BancoFacade.verificarAprovacaoEmprestimo(this, valor);
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{numero='" + numero + "', titular='" + titular + "', saldo=" + saldo + "}";
+    }
 }
