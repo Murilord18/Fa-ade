11bc70396dd6d3026d5d902d775f9d99b3c8f09a
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:07:55 -0300

    Update Cobranca.

--- src/main/java/br/com/banco/facade/Cobranca.java
+++ src/main/java/br/com/banco/facade/Cobranca.java
@@ -1,4 +1,17 @@
 package br.com.banco.facade;
 
-public class Cobranca {
+public class Cobranca extends Departamento{
+
+    private static final Cobranca instancia = new Cobranca();
+
+    private Cobranca() {}
+
+    public static Cobranca getInstancia() {
+        return instancia;
+    }
+
+    @Override
+    public String getNome() {
+        return "Departamento de Cobrança";
+    }
 }
